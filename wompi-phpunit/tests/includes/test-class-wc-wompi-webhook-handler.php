67a1a1cf9bc53610c83828268b05e63c954bef6e
PaulSamsonov <samsonov.teamwork@gmail.com>
2020-01-13 19:24:50 +0200

    Full name checking changes and comments removes

--- wompi-phpunit/tests/includes/test-class-wc-wompi-webhook-handler.php
+++ wompi-phpunit/tests/includes/test-class-wc-wompi-webhook-handler.php
@@ -33,16 +33,15 @@ class Test_WC_Wompi_Webhook_Handler extends WC_Wompi_Webhook_Handler {
         // Check order status
         $status = $order->get_status();
         if ( $status == 'completed' ) {
-            // Parse full name
-            $full_name = WC_Wompi_Helper::split_fullname( $transaction->customer_data->full_name );
             // Check transaction id
             TestCase::assertEquals( $transaction->id, $order->get_transaction_id() );
             // Check customer email
             TestCase::assertEquals( $transaction->customer_email, get_post_meta($order_id, '_billing_email', true) );
+            TestCase::assertEquals( $transaction->customer_email, get_post_meta($order_id, '_billing_address_index', true) );
             // Check first name
-            TestCase::assertEquals( $full_name[0], get_post_meta($order_id, '_billing_first_name', true) );
+            TestCase::assertEquals( $transaction->customer_data->full_name, get_post_meta($order_id, '_billing_first_name', true) );
             // Check last name
-            TestCase::assertEquals( $full_name[1], get_post_meta($order_id, '_billing_last_name', true) );
+            TestCase::assertEquals( '', get_post_meta($order_id, '_billing_last_name', true) );
             // Check phone number
             TestCase::assertEquals( $transaction->customer_data->phone_number, get_post_meta($order_id, '_billing_phone', true) );
         } else {
